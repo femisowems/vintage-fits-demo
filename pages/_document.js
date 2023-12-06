5250882843416e18fc945efecf43053ce9958bff
Femi Sowemimo <femi.sowems@gmail.com>
2023-12-06 00:18:53 -0500

    feat: saving progress

---
+++ pages/_document.js
@@ -0,0 +1,12 @@
+export default class MyDocumnet extends Document {
+
+    render () {
+        <html lang="en-ca">
+        <body>
+            <Main />
+                { <Head></Head>  }   
+                <NextScript />
+        </body>
+        </html>
+    }
+}
---
+++ pages/sell.js
@@ -0,0 +1,6 @@
+import Page from "../components/Page";
+
+export default function SellPage() {
+    return <p> Hello I am the sell page! </p>
+
+} 
